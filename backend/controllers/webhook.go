45483dd5adb638ff2b2a661e4fc023116942b2e3
Mark Albrand <mark.albrand5@gmail.com>
2024-02-20 11:42:00 -0600

    Insertar en la base de datos

--- backend/controllers/webhook.go
+++ backend/controllers/webhook.go
@@ -1,12 +1,17 @@
 package controllers
 
 import (
+	"Boquiteo-Backend/configs"
+	"Boquiteo-Backend/models"
 	"Boquiteo-Backend/responses"
 	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"io"
 	"net/http"
+	"strings"
+	"time"
 )
 
 // ReceiveWebhook receives the webhook from Shopify or Resttouch
@@ -90,4 +95,78 @@ func ReceiveWebhook(c *gin.Context) {
 		return
 	}
 	fmt.Println("Customer: " + customer)
+
+	var lineItems []interface{} // [{"name": "item1", "quantity": 1, "price": 10.00, "vendor": "vendor1"}, ...]
+
+	if jsonLineItems, ok := jsonBody["line_items"]; ok {
+		for _, item := range jsonLineItems.([]interface{}) {
+			itemName := item.(map[string]interface{})["name"]
+			itemQuantity := item.(map[string]interface{})["quantity"]
+			itemPrice := item.(map[string]interface{})["price"]
+			itemVendor := item.(map[string]interface{})["vendor"]
+
+			lineItems = append(lineItems, map[string]interface{}{
+				"name":     itemName,
+				"quantity": itemQuantity,
+				"price":    itemPrice,
+				"vendor":   itemVendor,
+			})
+		}
+	}
+
+	fmt.Println("Line items: ", lineItems)
+
+	var address string
+	// get jsonBody["customer"]["default_address"]
+	if jsonDefaultAddress, ok := jsonBody["customer"].(map[string]interface{})["default_address"]; ok {
+		// if exists, get address1 and address2 and concatenate them to address
+		if jsonAddress1, ok := jsonDefaultAddress.(map[string]interface{})["address1"]; ok {
+			// strip the address of any commas and blank spaces
+			address = jsonAddress1.(string)
+		}
+		if jsonAddress2, ok := jsonDefaultAddress.(map[string]interface{})["address2"]; ok {
+			address += ", " + jsonAddress2.(string)
+		}
+		if jsonCity, ok := jsonDefaultAddress.(map[string]interface{})["city"]; ok {
+			address += ". " + jsonCity.(string)
+		}
+	}
+	address = strings.ReplaceAll(address, "  ", " ") // replace double spaces with single space
+	fmt.Println("Address: ", address)
+
+	orderConfirmedTime := primitive.DateTime(time.Now().UnixNano() / int64(time.Millisecond))
+	fmt.Println("Order confirmed time: ", orderConfirmedTime)
+
+	// Insertar a la base de datos
+	var newOrder models.Order
+
+	newOrder.OrderNumber = orderNumber
+	newOrder.TotalPrice = totalPrice
+	newOrder.Customer = customer
+	newOrder.LineItems = lineItems
+	newOrder.Address = address
+	newOrder.TimeOrderConfirmed = orderConfirmedTime
+	newOrder.ShopifyDetails = jsonBody
+
+	// Insertar a la base de datos
+	collection := configs.GetCollection(configs.DB, "orders")
+	insertResult, err := collection.InsertOne(c, newOrder)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, responses.StandardResponse{
+			Status:  http.StatusInternalServerError,
+			Message: "Error while inserting the order to the database",
+			Data:    nil,
+		})
+		return
+	}
+
+	fmt.Println("Inserted a single document: ", insertResult.InsertedID)
+
+	c.JSON(http.StatusOK, responses.StandardResponse{
+		Status:  http.StatusOK,
+		Message: "Order inserted to the database",
+		Data: map[string]interface{}{
+			"order_id": insertResult.InsertedID,
+		},
+	})
 }
--- backend/models/order.go
+++ backend/models/order.go
@@ -4,9 +4,12 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 // Order struct
 type Order struct {
-	ID             primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	OrderNumber    int                `json:"order_number" bson:"order_number"`
-	TotalPrice     string             `json:"total_price" bson:"total_price"`
-	Customer       string             `json:"customer" bson:"customer"`
-	ShopifyDetails interface{}        `json:"shopify_details" bson:"shopify_details"`
+	ID                 primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	OrderNumber        int                `json:"order_number" bson:"order_number"`
+	TotalPrice         string             `json:"total_price" bson:"total_price"`
+	Customer           string             `json:"customer" bson:"customer"`
+	LineItems          []interface{}      `json:"line_items" bson:"line_items"`
+	Address            string             `json:"address" bson:"address"`
+	TimeOrderConfirmed primitive.DateTime `json:"time_order_confirmed" bson:"time_order_confirmed"`
+	ShopifyDetails     interface{}        `json:"shopify_details" bson:"shopify_details"`
 }
